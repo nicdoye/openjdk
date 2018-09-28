aa6f8e0268cf1dc9ff33c05dd09f5ca956b49431
Nic Doye <nic.doye@alfresco.com>
2018-09-28 14:45:15 +0100

    DEPLOY-574 Final dummy build

--- build-prep.sh
+++ build-prep.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 # shellcheck source=./build.properties
 source ./build.properties
+# shellcheck source=./checksum.properties
+source=./checksum.properties
 
 declare -r DOWNLOAD_DIR='.'
 
@@ -18,11 +20,15 @@ JRE_CHECKSUM_256_REF="JRE_CHECKSUM_256_${JAVA_VERSION_CLEANED}"
 # Alfresco Repo
 # Stored like "linux-x64:serverjre:8u181:tar.gz:bin"
 MVN_REPO_URL=https://artifacts.alfresco.com/nexus/content/repositories/oracle-java
+# Mavenise me
 GROUP_ID="${JAVA_OS_ARCH}"
 ARTIFACT_ID="${JAVA_SE_TYPE}"
 JAVA_BASENAME="${JAVA_SE_TYPE}-${JAVA_VERSION}-bin.${JAVA_PACKAGING}"
 
-java::download::mvn () {
+CHECKSUM="${CHECKSUM_256["${GROUP_ID}":"${ARTIFACT_ID}":"${JAVA_VERSION}":"${JAVA_PACKAGING}"]}"
+
+
+java::download::artifacts::mvn () {
     # As Oracle have made downloading non-current versions of Java difficult,
     # we are sadly having to store them in an internal repository.
 
@@ -40,7 +46,7 @@ java::download::mvn () {
     JRE_FILENAME="${DOWNLOAD_DIR}/${JAVA_BASENAME}"
 }
 
-java::download::artifacts () {
+java::download::artifacts::curl () {
     # Also uses artifacts.alfresco.com but via curl
     declare OLD_PWD="${PWD}"
     cd "${DOWNLOAD_DIR}"
@@ -49,7 +55,20 @@ java::download::artifacts () {
     JRE_FILENAME="${DOWNLOAD_DIR}/${JAVA_BASENAME}"
 }
 
-java::download::oracle () {
+java::download::openjdk::curl () {
+    # You can still download the latest Java version from Oracle
+
+    declare OLD_PWD="${PWD}"
+    cd "${DOWNLOAD_DIR}"
+    # https://download.java.net/java/GA/jdk11/28/GPL/openjdk-11+28_linux-x64_bin.tar.gz
+    # https://download.java.net/java/GA/jdk11/28/GPL/openjdk-11+28_linux-x64_bin.tar.gz.sha256
+    curl -sSLO "${JRE_URL}"
+    cd "${OLD_PWD}"
+
+    JRE_FILENAME="${DOWNLOAD_DIR}/${JRE_URL##*/}"
+}
+
+java::download::oracle::curl () {
     # You can still download the latest Java version from Oracle
 
     declare OLD_PWD="${PWD}"
@@ -63,11 +82,13 @@ java::download::oracle () {
 # Wrapper
 java::download () {
     if [ "${USE_MVN}" = 'true' ]; then
-        java::download::mvn
+        java::download::artifacts::mvn
     elif [ "${USE_CURL}" = 'true' ]; then
-        java::download::artifacts
+        java::download::artifacts::curl; then
+    elif [ "${USE_OPENJDK}" = 'true' ]; then
+        java::download::openjdk::curl
     else
-        java::download::oracle
+        java::download::oracle::curl
     fi
 }
 
@@ -76,11 +97,11 @@ java::checksum () {
     # Note: two spaces are required between the variables in some implementations
 
     if [ -x "$(command -v sha256sum)" ]; then
-        echo "${!JRE_CHECKSUM_256_REF}  ${JRE_FILENAME}" | sha256sum -c - > /dev/null
+        echo "${CHECKSUM}  ${JRE_FILENAME}" | sha256sum -c - > /dev/null
     elif [ -x "$(command -v gsha256sum)" ]; then
-        echo "${!JRE_CHECKSUM_256_REF}  ${JRE_FILENAME}" | gsha256sum -c - > /dev/null
+        echo "${CHECKSUM}  ${JRE_FILENAME}" | gsha256sum -c - > /dev/null
     else
-        echo "${!JRE_CHECKSUM_256_REF}  ${JRE_FILENAME}" | shasum -a 256 -c - > /dev/null
+        echo "${CHECKSUM}  ${JRE_FILENAME}" | shasum -a 256 -c - > /dev/null
     fi
 }
 
